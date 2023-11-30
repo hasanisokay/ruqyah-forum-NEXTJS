6d02247b94a67f582d34abc1e42adb1d3b15d402
hasanisokay <hasanisokay@gmail.com>
2023-11-30 14:41:44 +0600

    edited db?

--- src/app/api/getnotice/route.js
+++ src/app/api/getnotice/route.js
@@ -2,9 +2,13 @@ import dbConnect from "@/services/DbConnect"
 import { NextResponse } from "next/server"
 
 export const GET = async ()=>{
-
     const db = await dbConnect();
     const noticeCollection = db?.collection("notice");
-    const result = await noticeCollection.find().sort({date:-1}).toArray();
+   try{
+    const result = await noticeCollection?.find({}).sort({date:-1}).toArray();
     return NextResponse.json(result)
+   }
+   catch{
+    return NextResponse.json({status:500, message:"Error"})
+   }
 }
