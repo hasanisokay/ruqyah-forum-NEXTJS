ba2c836aef76500bf986e0a1a8875bc10640c58e
hasanisokay <hasanisokay@gmail.com>
2023-12-02 15:05:55 +0600

    added aws link

--- src/services/socket.js
+++ src/services/socket.js
@@ -4,7 +4,7 @@ import { io } from 'socket.io-client';
 const initializeSocket = async () => {
   try {
     // const serverUrl = 'http://localhost:3001';
-    const serverUrl = 'ws://13.229.79.153:3001';
+    const serverUrl = 'wss://13.229.79.153:3001';
     const { user } = await getUser();
     const socket = io(serverUrl, {
       query: {
