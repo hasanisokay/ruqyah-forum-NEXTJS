b95fb8afe7efcf165b7540d478e99f032acc4909
hasanisokay <hasanisokay@gmail.com>
2024-01-02 10:31:34 +0600

    video link for youtube updated

--- src/components/video-components/VideosInPost.jsx
+++ src/components/video-components/VideosInPost.jsx
@@ -8,7 +8,7 @@ const VideosInPost = ({ videosArray }) => {
     <div className="max-w-[100vw] min-w-[300px] lg:min-h-[300px]">
       {videosArray.map((videoUrl, index) => (
         <div key={index} >
-          {videoUrl.includes('youtube.com') ? (
+          {(videoUrl.includes('youtube.com') || videoUrl.includes("youtu.be")) ? (
             <YouTubeVideo videoUrl={videoUrl} />
           ) : videoUrl.includes('drive.google.com') ? (
             <GoogleDriveVideo videoUrl={videoUrl} />
